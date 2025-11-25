af58839932fffea5cc3f4f212a5bcd9da457b845
ASHFAQ-91 <ashfaqqureshi242@gmail.com>
2025-11-25 23:16:44 +0530

    Added new line of code

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,16 @@
 // npm install express mongoose jsonwebtoken bcryptjs dotenv cookie-parser nodemon
 // npm install --save-dev nodemon
 import express from "express";
+import dotenv from "dotenv";
 import "dotenv/config"
+import authRouter from "./routes/authRoutes.js";
 
 // Initialize Express App
 const app = express()
 
 // Routes
-app.get('/', (req, res) => res.send("Server is running"));  // on the webpage
-app.get('/api/auth/signup', (req, res) => res.send("Signup Route"));  // on the webpage
-app.get('/api/auth/login', (req, res) => res.send("Login Route"));  // on the webpage
-app.get('/api/auth/logout', (req, res) => res.send("Logout Route"));  // on the webpage
+app.use('/api/auth', authRouter);   
+
 
 
 
@@ -20,3 +20,4 @@ app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))  // on the
 
 //$ npm run server - to start the project.
 // By using "process.env.PORT || 3000", you create a single codebase that functions seamlessly both locally (on port 3000) and in production environments (using the required dynamic port assigned by services like Heroku, AWS Elastic Beanstalk, or Azure App Service).
+// Primary file you can name = "server.js" or "index.js" or "app.js" or "main.js" or "backend.js".
